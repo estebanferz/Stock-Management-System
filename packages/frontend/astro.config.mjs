8f93a19b6a437fc303b8435d3de30e7507f8e690
Esteban Fernandez <fernandezesteban344@gmail.com>
2025-11-08 19:21:47 -0300

    start frontend
    add Header & basic Layout

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Importa las integraciones que instalaste
+import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Define el modo de renderizado a "servidor" (SSR)
+  output: 'server',
+  adapter: node({ mode: "standalone" }),
+
+  // Configura las integraciones
+  integrations: [
+    // 1. Integración de Tailwind
+    tailwind(),
+  ],
+});
