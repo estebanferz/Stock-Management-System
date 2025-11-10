2363eef4355385cd11032b2d2bd3f1a7ba70dea5
Esteban Fernandez <fernandezesteban344@gmail.com>
2025-11-10 00:27:43 -0300

    quick checkpoint Sales page

---
+++ packages/frontend/src/components/Structures/paymentMethods.ts
@@ -0,0 +1,6 @@
+export const paymentMethods = [
+  { value: "transferencia", label: "Transferencia"},
+  { value: "efectivo-usd", label: "Efectivo USD" },
+  { value: "efectivo-ars", label: "Efectivo ARS" },
+  { value: "crypto", label: "Crypto" },
+];
