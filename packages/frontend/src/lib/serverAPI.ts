75cd6bf621284e7f6e9be2096cfe65f34ec11462
Esteban Fernandez <fernandezesteban344@gmail.com>
2025-12-22 13:15:38 -0300

    Deploy frontend: fixed proxy (II HAVE BEEN CHANGED... FOR GOOD)

--- packages/frontend/src/lib/serverAPI.ts
+++ packages/frontend/src/lib/serverAPI.ts
@@ -1,15 +1,9 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@server/src/index";
 
-const getBrowserOrigin = () => {
-  if (typeof window !== "undefined" && window.location?.origin) {
-    return window.location.origin;
-  }
-  return "";
-};
-
-const origin = import.meta.env.SSR
-  ? (process.env.SERVER_API_URL ?? "")
-  : getBrowserOrigin();
+const origin =
+  import.meta.env.SSR
+    ? process.env.SERVER_API_BASE!.replace(/\/api$/, "")
+    : window.location.origin;
 
 export const serverApp = treaty<App>(origin).api;
