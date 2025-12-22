c621a2d54c781fe890d3c294575a05a2ac61a4d3
Esteban Fernandez <fernandezesteban344@gmail.com>
2025-12-22 12:12:40 -0300

    Deploy frontend: fixed clientAPI

--- packages/frontend/src/lib/clientAPI.ts
+++ packages/frontend/src/lib/clientAPI.ts
@@ -1,9 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@server/src/index";
 
+// En browser: same-origin (NO /api porque ya lo pone `.api`)
 const baseURL = import.meta.env.SSR
-  ? (process.env.SERVER_API_URL ?? import.meta.env.PUBLIC_API_URL) // server-side
-  : "/api"; // browser-side (same-origin)
+  ? (process.env.SERVER_API_ORIGIN ?? import.meta.env.PUBLIC_API_ORIGIN)
+  : "";
 
 export const clientApp = treaty<App>(baseURL, {
   fetch: { credentials: "include" },
