868ff149488601751e85066154c2c31bbe0e68f7
Esteban Fernandez <fernandezesteban344@gmail.com>
2025-12-22 12:38:04 -0300

    Deploy frontend: fixed clientAPI v3, serveAPI

--- packages/frontend/src/lib/serverAPI.ts
+++ packages/frontend/src/lib/serverAPI.ts
@@ -1,4 +1,15 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@server/src/index";
 
-export const serverApp = treaty<App>(import.meta.env.SERVER_API_URL).api;
+const getBrowserOrigin = () => {
+  if (typeof window !== "undefined" && window.location?.origin) {
+    return window.location.origin;
+  }
+  return "";
+};
+
+const origin = import.meta.env.SSR
+  ? (process.env.SERVER_API_URL ?? "")
+  : getBrowserOrigin();
+
+export const serverApp = treaty<App>(origin).api;
